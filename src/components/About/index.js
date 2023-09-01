e9216ef78053a6e8483f3e5fe9d8f1a88b87e361
Starmoozie <starmoozie@gmail.com>
2023-09-01 11:16:37 +0700

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -47,6 +47,10 @@ const About = () => {
             </p>
             <br />
             <p>
+              Kejar jam tayang YouTube / Akun monetesisasi YouTube / Kejar $10 pertama juga siaappp...
+            </p>
+            <br />
+            <p>
               Dibawah ini adalah beberapa framework / bahasa pemograman yang saya
               kuasai:
             </p>
